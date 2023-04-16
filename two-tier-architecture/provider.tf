2bbc5be78b2299c151f092356c65b4714b8d3c12
Abhiramgc <abhiram.gannavaram@gmail.com>
2023-04-16 18:37:32 +0530

    Modified gh-wf

--- two-tier-architecture/provider.tf
+++ two-tier-architecture/provider.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~>4"
+      version = "~> 4.63.0"
     }
   }
 }
 
 provider "aws" {
-  region  = "us-east-1"
+  region = "us-east-1"
 }
